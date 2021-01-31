475291d3f5ffe3ac17db37c85e415b58745a4893
musibs <musib.somnath@gmail.com>
2021-01-31 19:14:30 +0530

    Implementing Inmemory authentication

---
+++ ch05/Implementing-inmemory-authentication/course-tracker-final/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS courses;
+CREATE TABLE courses (
+  id          BIGINT NOT NULL auto_increment,
+  category    VARCHAR(255),
+  description VARCHAR(255),
+  name        VARCHAR(255),
+  rating      INTEGER NOT NULL,
+  PRIMARY KEY (id)
+) engine=innodb;
---
+++ ch05/Implementing-inmemory-authentication/course-tracker-start/src/main/java/com/manning/sbip/ch04/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.manning.sbip.ch04.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
