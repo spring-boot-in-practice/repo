88923c278e0398de8e02652e11ebb8a4ed24a470
musibs <musib.somnath@gmail.com>
2021-02-10 12:02:52 +0530

    Spring Boot default logging

---
+++ ch02/spring-boot-default-logging/spring-boot-app-final/src/main/java/com/manning/sbip/ch02/CourseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.manning.sbip.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseTrackerApplication.class, args);
+    }
+
+}
