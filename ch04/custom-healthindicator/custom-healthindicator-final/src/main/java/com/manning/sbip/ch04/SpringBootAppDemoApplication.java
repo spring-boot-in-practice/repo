3f436f914a8df23f423300cd285ef2f730bd5024
musibs <musib.somnath@gmail.com>
2021-03-27 17:01:00 +0530

    Added custom health indicator

---
+++ ch04/custom-healthindicator/custom-healthindicator-final/src/main/java/com/manning/sbip/ch04/SpringBootAppDemoApplication.java
@@ -0,0 +1,12 @@
+package com.manning.sbip.ch04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppDemoApplication.class, args);
+	}
+}
---
+++ ch04/custom-healthindicator/custom-healthindicator-final/src/main/java/com/manning/sbip/ch04/health/indicator/DogsApiHealthIndicator.java
@@ -0,0 +1,34 @@
+package com.manning.sbip.ch04.health.indicator;
+
+import java.util.Map;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class DogsApiHealthIndicator implements HealthIndicator {
+		
+	public Health health() {
+		try {
+			ParameterizedTypeReference<Map<String, String>> reference = new ParameterizedTypeReference<Map<String, String>>() {};
+	        ResponseEntity<Map<String, String>> result = new RestTemplate().exchange("https://dog.ceo/api/breeds/image/random", HttpMethod.GET, null, reference);
+	        if (result.getStatusCode().is2xxSuccessful() && result.getBody() != null) {
+	        	return Health.up().withDetails(result.getBody()).build();
+	        } 
+	        else {
+	        	return Health.down().withDetail("status", result.getStatusCode()).build();
+	        }
+		}
+		catch(RestClientException ex) {
+			 return Health.down().withException(ex).build();
+		}
+	}
+	
+
+}
