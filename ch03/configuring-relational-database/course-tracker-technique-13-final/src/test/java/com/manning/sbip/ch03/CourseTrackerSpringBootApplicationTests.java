733eab610c65535f0199575a369ebe5736f0f12b
musibs <musib.somnath@gmail.com>
2021-04-08 18:39:34 +0530

    Added Configuring MongoDb database

---
+++ ch03/configuring-relational-database/course-tracker-technique-13-final/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
@@ -0,0 +1,27 @@
+package com.manning.sbip.ch03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class CourseTrackerSpringBootApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void givenDatasourceAvailableWhenAccessDetailsThenExpectDetails() throws SQLException {
+        assertThat(dataSource.getClass().getName()).isEqualTo("com.zaxxer.hikari.HikariDataSource");
+        assertThat(dataSource.getConnection().getMetaData().getDatabaseProductName()).isEqualTo("MySQL");
+    }
+}
