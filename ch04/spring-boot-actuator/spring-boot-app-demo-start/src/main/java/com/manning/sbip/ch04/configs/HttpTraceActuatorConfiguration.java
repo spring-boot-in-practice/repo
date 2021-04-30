7ebe93aa3b23a1028cfbcb7843eee3bf6d440340
musibs <musib.somnath@gmail.com>
2021-04-30 10:23:20 +0530

    Ch04 Spring Boot Actuator changes

---
+++ ch04/spring-boot-actuator/spring-boot-app-demo-start/src/main/java/com/manning/sbip/ch04/configs/HttpTraceActuatorConfiguration.java
@@ -0,0 +1,15 @@
+package com.manning.sbip.ch04.configs;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpTraceActuatorConfiguration {
+
+	@Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }	
+}
