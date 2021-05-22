699f21f71b9199fec988c878ef93732af4c300e1
musibs <musib.somnath@gmail.com>
2021-05-22 17:55:35 +0530

    Sign in with Google

---
+++ ch06/sigin-with-google/course-tracker-final/src/main/java/com/manning/sbip/ch06/security/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.manning.sbip.ch06.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.manning.sbip.ch06.handler.CustomAuthenticationFailureHandler;
+import com.manning.sbip.ch06.service.impl.Oauth2AuthenticationSuccessHandler;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/adduser", "/login", "/login-error", "/login-verified", "/login-disabled", "/verify/email")
+				.permitAll().anyRequest().authenticated()
+				.and()
+				.formLogin().loginPage("/login").failureHandler(customAuthenticationFailureHandler)
+				.and()
+				.oauth2Login().loginPage("/login").successHandler(new Oauth2AuthenticationSuccessHandler());
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/webjars/**", "/images/*", "/css/*", "/h2-console/**");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
