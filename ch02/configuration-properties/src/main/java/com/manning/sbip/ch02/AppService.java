e7ed6bac6c0ffa48bde3c2df4edcbdaa248a9c20
Somnath Musib <7885767+musibs@users.noreply.github.com>
2021-11-10 10:53:47 +0530

    P2 review changes for chapter 2

--- ch02/configuration-properties/src/main/java/com/manning/sbip/ch02/AppService.java
+++ ch02/configuration-properties/src/main/java/com/manning/sbip/ch02/AppService.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class AppService {
 
+    private final AppProperties appProperties;
+
     @Autowired
-    private AppProperties appProperties;
+    public AppService(AppProperties appProperties) {
+        this.appProperties = appProperties;
+    }
 
     public AppProperties getAppProperties() {
         return this.appProperties;
--- ch02/configuration-properties/src/main/java/com/manning/sbip/ch02/SpringBootAppDemoApplication.java
+++ ch02/configuration-properties/src/main/java/com/manning/sbip/ch02/SpringBootAppDemoApplication.java
@@ -1,10 +1,10 @@
 package com.manning.sbip.ch02;
 
-import com.manning.sbip.ch02.configurationproperties.AppProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
@@ -12,10 +12,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 @ConfigurationPropertiesScan
 public class SpringBootAppDemoApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootAppDemoApplication.class);
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootAppDemoApplication.class, args);
 		AppService appService = applicationContext.getBean(AppService.class);
-		System.out.println(appService.getAppProperties());
+		log.info(appService.getAppProperties().toString());
 
 	}
 }
