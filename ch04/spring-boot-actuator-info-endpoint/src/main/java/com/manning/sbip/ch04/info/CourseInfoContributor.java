6d708781ae1ccfade1fd58ac6fbe6c122ab7d2e7
Somnath Musib <7885767+musibs@users.noreply.github.com>
2022-01-28 13:48:27 +0530

    'P3 Production review changes

--- ch04/spring-boot-actuator-info-endpoint/src/main/java/com/manning/sbip/ch04/info/CourseInfoContributor.java
+++ ch04/spring-boot-actuator-info-endpoint/src/main/java/com/manning/sbip/ch04/info/CourseInfoContributor.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Component
+//@Component
 public class CourseInfoContributor implements InfoContributor {
 
     private CourseService courseService;
